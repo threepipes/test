a7aa65f56734d11d7697d7e92ce2fa0add9da562
threepipes_s <threepipesatos@gmail.com>
2015-08-17 17:19:47 +0900

    add prime func

--- gittest.py
+++ gittest.py
@@ -1,2 +1,11 @@
 n = 100
+for i in range(10):
+    n += 10
 print(n*n)
+a = [i for i in range(2, n+1)]
+for i in a:
+    j = i<<1
+    while j <= n:
+        if j in a: a.remove(j)
+        j += i
+print(a)
